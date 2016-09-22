fe6450161ee007c5da5daa8a32c397c1504c977d
Dylan Hutchison <dhutchis@stevens.edu>
2016-09-21 19:46:09 -0700

    Splice PTree into a separate file

--- src/test/kotlin/edu/washington/cs/laragraphulo/opt/raco/RacoConvertTest.kt
+++ src/test/kotlin/edu/washington/cs/laragraphulo/opt/raco/RacoConvertTest.kt
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import java.io.StringReader
 import java.nio.ByteBuffer
+import edu.washington.cs.laragraphulo.opt.raco.PTree.*
 
 @RunWith(Parameterized::class)
 class RacoConvertTestPrelim(
@@ -24,7 +25,7 @@ class RacoConvertTestPrelim(
   @Test
   fun testPTreeParse() {
     val inps = StringReader(params.repr).buffered()
-    val parsed = parseRaco(inps)
+    val parsed = PTree.parseRaco(inps)
     Assert.assertEquals(params.expected, parsed)
   }
 
@@ -33,73 +34,73 @@ class RacoConvertTestPrelim(
         Params(
             name = "simple string",
             repr = "'hello world '",
-            expected = PTree.PString("hello world ")
+            expected = PString("hello world ")
         ),
         Params(
             name = "simple long",
             repr = "  42    ",
-            expected = PTree.PLong(42)
+            expected = PLong(42)
         ),
         Params(
             name = "complicated long",
             repr = " -000765",
-            expected = PTree.PLong(-765)
+            expected = PLong(-765)
         ),
         Params(
             name = "complicated double",
             repr = " -00.0765e2",
-            expected = PTree.PDouble(-7.65)
+            expected = PDouble(-7.65)
         ),
         Params(
             name = "list 1",
             repr = "  [ \" hi \", 27 , 32 ]",
-            expected = PTree.PList(listOf(
-                PTree.PString(" hi "), PTree.PLong(27), PTree.PLong(32)
+            expected = PList(listOf(
+                PString(" hi "), PLong(27), PLong(32)
             ))
         ),
         Params(
             name = "list + pair",
             repr = "  [ \" hi \", (27, 'ok') , 32 ]",
-            expected = PTree.PList(listOf(
-                PTree.PString(" hi "), PTree.PPair(PTree.PLong(27), PTree.PString("ok")), PTree.PLong(32)
+            expected = PList(listOf(
+                PString(" hi "), PPair(PLong(27), PString("ok")), PLong(32)
             ))
         ),
         Params(
             name = "list o list",
             repr = "  [ \" hi \", [(27, 'ok')] , 32 ]",
-            expected = PTree.PList(listOf(
-                PTree.PString(" hi "),
-                PTree.PList(listOf(PTree.PPair(PTree.PLong(27), PTree.PString("ok")))),
-                PTree.PLong(32)
+            expected = PList(listOf(
+                PString(" hi "),
+                PList(listOf(PPair(PLong(27), PString("ok")))),
+                PLong(32)
             ))
         ),
         Params(
             name = "list o maps",
             repr = "  [ {\" hi \" : 99}, [(27, 'ok')] , {'inlist': [32]} ,{}]",
-            expected = PTree.PList(listOf(
-                PTree.PMap(mapOf(" hi " to PTree.PLong(99))),
-                PTree.PList(listOf(PTree.PPair(PTree.PLong(27), PTree.PString("ok")))),
-                PTree.PMap(mapOf("inlist" to PTree.PList(listOf(PTree.PLong(32))))),
-                PTree.PMap(mapOf())
+            expected = PList(listOf(
+                PMap(mapOf(" hi " to PLong(99))),
+                PList(listOf(PPair(PLong(27), PString("ok")))),
+                PMap(mapOf("inlist" to PList(listOf(PLong(32))))),
+                PMap(mapOf())
             ))
         ),
         Params(
             name = "0-arg",
             repr = "Dump()",
-            expected = PTree.PNode("Dump", listOf())
+            expected = PNode("Dump", listOf())
         ),
         Params(
             name = "1-arg",
             repr = "Something('hey hey')",
-            expected = PTree.PNode("Something", listOf(PTree.PString("hey hey")))
+            expected = PNode("Something", listOf(PString("hey hey")))
         ),
         Params(
             name = "3-arg nested",
             repr = "Something3('hey hey', Dump(), Something(42))",
-            expected = PTree.PNode("Something3", listOf(
-                PTree.PString("hey hey"),
-                PTree.PNode("Dump", listOf()),
-                PTree.PNode("Something", listOf(PTree.PLong(42)))
+            expected = PNode("Something3", listOf(
+                PString("hey hey"),
+                PNode("Dump", listOf()),
+                PNode("Something", listOf(PLong(42)))
             ))
         )
     )
@@ -109,6 +110,11 @@ class RacoConvertTestPrelim(
     fun parameters(): Array<out Any> = tests
   }
 }
+
+
+
+
+
 
 class RacoConvertTest(
     val params: Params
